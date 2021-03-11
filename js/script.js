ff392e10b64a6f5b64fc9350a19348dea6451663
Mikhail Vdovareize <mvdovare@gmail.com>
2021-03-11 18:04:54 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 console.log('Hello!');
+console.log('WORLD');
+console.log('123');
+console.log(123);
+console.log('Hello!'+'world');
+console.log('Hello!'+' world');
+console.log('Hello! '+'world');
+console.log('Hello!'+' '+'world');
+console.info('info');
+// alert('ворнигн!');
+// alert('ворнигн!');
+
+document.getElementById('out').innerHTML = 'HELLO';
+document.getElementById('out').innerHTML = 2021;
+document.getElementById('out').innerHTML = '<b>2020</b>';
+
+document.querySelector('.header').innerHTML = 5;
+document.querySelector('#one').innerHTML = 777; // есть решетка
+document.getElementById('one').innerHTML = 888; // нет решетки
+
+let b;
+let a = document.querySelector('#one'); //
+let c;
+c = document.querySelector('.header');
+
+a.innerHTML = 999; // 
+c.innerHTML = 6666;
