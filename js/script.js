2cab04f5460c129e8bd0e38c3dc6a00355c615c1
Mikhail Vdovareize <mvdovare@gmail.com>
2021-03-12 18:09:39 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,54 @@
-console.log('Hello!');
-console.log('WORLD');
-console.log('123');
-console.log(123);
-console.log('Hello!'+'world');
-console.log('Hello!'+' world');
-console.log('Hello! '+'world');
-console.log('Hello!'+' '+'world');
-console.info('info');
+// console.log('Hello!');
+// console.log('WORLD');
+// console.log('123');
+// console.log(123);
+// console.log('Hello!'+'world');
+// console.log('Hello!'+' world');
+// console.log('Hello! '+'world');
+// console.log('Hello!'+' '+'world');
+// console.info('info');
 // alert('ворнигн!');
 // alert('ворнигн!');
 
-document.getElementById('out').innerHTML = 'HELLO';
-document.getElementById('out').innerHTML = 2021;
-document.getElementById('out').innerHTML = '<b>2020</b>';
+// document.getElementById('out').innerHTML = 'HELLO';
+// document.getElementById('out').innerHTML = 2021;
+// document.getElementById('out').innerHTML = '<b>2020</b>';
 
-document.querySelector('.header').innerHTML = 5;
-document.querySelector('#one').innerHTML = 777; // есть решетка
-document.getElementById('one').innerHTML = 888; // нет решетки
+// document.querySelector('.header').innerHTML = 5;
+// document.querySelector('#one').innerHTML = 777; // есть решетка
+// document.getElementById('one').innerHTML = 888; // нет решетки
 
-let b;
-let a = document.querySelector('#one'); //
-let c;
-c = document.querySelector('.header');
+// let b;
+// let a = document.querySelector('#one'); //
+// // let c;
+// c = document.querySelector('.header');
 
-a.innerHTML = 999; // 
-c.innerHTML = 6666;
+// a.innerHTML = 999; // 
+// c.innerHTML = 6666;
+let a = 7;
+let b = 9;
+let c = 7;
+let d = 9;
+let e = 3;
+let f = 5;
+let div = document.querySelector('.out');
+let div2 = document.querySelector('.out2');
+
+// console.log(a);
+
+/* let inPutIn = document.querySelector('.input-in'); // input
+/* let button = document.querySelector('button'); // button  */ 
+
+/* button.onclick = function(){
+    // когда кнопка будет нажата
+    console.log('работает');
+    // console.log( inPutIn.value ); // value - введено в инпут
+    let b = +inPutIn.value; // преобразовать в число
+    console.log(b + 10);
+    div.innerHTML = b;
+    inPutIn.value = '';
+} */
+
+console.log(a * b);
+div.innerHTML = c / d;
+div2.innerHTML = e + f;
