36c724cd61437fb0f50af1aebeb3edb2ab246a50
Mikhail Vdovareize <mvdovare@gmail.com>
2021-03-12 19:23:38 +0200

    commit

---
+++ js/script3.js
@@ -0,0 +1,41 @@
+/* let a = 9; */
+
+/* if (a != 9) {
+    // если выражение выполняется
+    console.log('yes');
+}
+else {
+    console.log('else');
+} */
+/* 
+const button = document.querySelector('button');
+const input = document.querySelector('.age');
+
+// () => стрелочная функция
+button.onclick = () => {
+    let num = +input.value;
+    if (num >= 16 && num < 60) {
+        console.log('Welcome!');
+    }
+    else if (num > 60){
+        console.log('старпер');
+    }
+    else {
+        console.log('Get out of here!');
+    }
+
+    switch (num) {
+        case 15:
+            console.log('недорос');
+            break;
+        case 16:
+            console.log('Ураа, дорос!');
+            break;
+    default:
+        console.log('goood');
+    }
+}
+
+let b = 3;
+
+console.log(b == 3 || b == 7); // оператор и || или */
---
+++ js/script3_dz.js
@@ -0,0 +1,21 @@
+// При нажатии кнопки b-1 срабатывает функция f1. 
+// Функция должна прочитать содержимое i-1 и сравнить его с числом 4 (сравнение ==). 
+// Результат сравнения - true или false выведите в out-1.
+
+
+const button = document.querySelector('button');
+const input = document.querySelector('.i-1');
+
+let div = document.querySelector('.out');
+let div2 = document.querySelector('.out2');
+
+// () => стрелочная функция
+button.onclick = () => {
+    let num = +input.value;
+    if (num == 4) {
+        div2.innerHTML = "True";
+    }
+     else {
+        div.innerHTML = "False";
+    }
+}
