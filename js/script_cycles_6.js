a517661edb7547cefb920c3fbb1daca6b884120b
Mikhail Vdovareize <mvdovare@gmail.com>
2021-03-25 08:10:56 +0200

    commit

---
+++ js/script_cycles_6.js
@@ -0,0 +1,40 @@
+let out = document.querySelector('.out');
+// for (let i = 0; i < 5; i++) {
+//     for (let k = 0; k < 10; k++){
+//         out.innerHTML += '*';
+//     }
+//     out.innerHTML += '<br>';
+// }
+
+// for (let i = 1; i < 10; i++){
+//     for (let k=1; k<10; k++){
+//         out.innerHTML += `${i}*${k}=${k*i}<br>`;
+//     }
+//     out.innerHTML += '<hr>';
+// }
+
+for (let i = 0; i < 3; i++){
+    for (let k = 0; k < 3; k++){
+        out.innerHTML +='*';
+    }
+    out.innerHTML +='_';
+}
+
+out.innerHTML += '<hr>';
+
+for (let i = 1; i < 4; i++){
+    out.innerHTML += i + '<br>';
+    for (let k = 0; k < 3; k++){
+        out.innerHTML += '*_';
+    }
+    out.innerHTML +='<br>';
+}
+
+out.innerHTML += '<hr>';
+
+for (let i = 0; i < 4; i++){
+    for (let k = 0; k < 3; k++){
+        out.innerHTML +='*_';
+    }
+    out.innerHTML +='<br>';
+}
