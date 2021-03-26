b563bee51486c26f83e2b2dd1ea3d58f347d506b
Mikhail Vdovareize <mvdovare@gmail.com>
2021-03-26 18:06:55 +0200

    commit

--- js/script_function_7.js
+++ js/script_function_7.js
@@ -5,7 +5,10 @@ function one(){
 
 one();
 
-f1.onclick = one; // без круглых скобок
+// f1.onclick = one; // без круглых скобок
+f1.onclick = () => {
+    console.log('Hello');
+}
 
 console.log(1 + one());
 console.log(one());
@@ -18,3 +21,40 @@ function two(){
 two();
 
 console.log(1 + two());
+
+let a = 8;
+let b = 9;
+
+function multi(){
+    console.log(a*b);
+    return a*b;
+}
+multi();
+let c1 = multi();
+let c2 = 10 * multi();
+console.log(c1,c2);
+
+function multi2(x=9, y=8){
+    return x * y;
+}
+
+
+console.log(multi2(4, 5));
+console.log(multi2(20, 5));
+console.log(multi2(20, a));
+console.log(multi2(10));
+
+document.querySelector('.f-2').onclick = function () {
+    console.log('+++++');
+}
+
+document.querySelector('.f-2').onclick = () =>{
+    console.log('arrow');
+    console.log('++++++++');
+}
+
+// function (a){
+// return 'Hello';
+// }
+
+// (a) => 'Hello';
